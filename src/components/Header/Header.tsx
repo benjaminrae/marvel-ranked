230ee77e22718fabaf542600bc0b58ec23903a32
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-09-17 01:55:44 +0200

    add styling to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,14 @@ import "./Header.css";
 import React from "react";
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div className="header">
+            <h1 className="header__title">
+                <span className="caps">Marvel</span>{" "}
+                <span className="border-top-bottom">Ranking</span>
+            </h1>
+        </div>
+    );
 };
 
 export default Header;
