1ab1d85c04cf3eef8e351704fbe0641ef8cb91cd
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-09-17 11:55:45 +0200

    add footer with correct attribution as per marvel documentation

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Data provided by Marvel. © 2014 Marvel</p>
+        </div>
+    );
+};
+
+export default Footer;
