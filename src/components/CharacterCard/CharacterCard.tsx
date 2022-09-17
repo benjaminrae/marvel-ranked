591e9e36ac123b253f7212384088cbc789d0adc7
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-09-17 04:56:32 +0200

    add basic character layout

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Button from "../Button/Button";
-import "./Character.css";
-import { Character } from "./services/marvel";
+import "./CharacterCard.css";
+import { Character } from "../../services/marvel";
 
 type CharacterProps = {
     character: Character;
 };
 const CharacterCard = ({ character }: CharacterProps): JSX.Element => {
     return (
-        <div>
+        <div className="character">
             <img
                 src={`${character.thumbnail.path}/portrait_uncanny.${character.thumbnail.extension}`}
                 alt={character.name}
+                className="character__image"
             />
-            <h2>{character.name}</h2>
+            <h2 className="character__title">{character.name}</h2>
+            <p className="character__description">{character.description}</p>
             <Button onClick={() => {}} innerText="vote" />
         </div>
     );
