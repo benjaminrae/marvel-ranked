f542611274519355d0393089192d09ff427d80d2
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-09-17 20:11:51 +0200

    create hamburger menu and open and close animations

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import "./Header.css";
 
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 type HeaderProps = {
     onLeaderboardClick: () => void;
@@ -10,23 +10,74 @@ const Header = ({
     onLeaderboardClick,
     onRankClick,
 }: HeaderProps): JSX.Element => {
+    const [showNav, setShowNav] = useState(true);
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+    const [width, setWidth] = useState(0);
+
+    const header = useRef<any>();
+
+    useEffect(() => {
+        updateWindowWidth();
+        window.addEventListener("resize", updateWindowWidth);
+    }, []);
+
+    useEffect(() => {
+        if (width < 630) {
+            setShowNav(false);
+            setHamburgerOpen(false);
+        } else {
+            setShowNav(true);
+            setHamburgerOpen(false);
+        }
+    }, [width]);
+
+    const updateWindowWidth = () => {
+        if (header.current) {
+            setWidth(header.current.clientWidth);
+        }
+    };
+
+    const toggleHamburger = () => {
+        setHamburgerOpen(!hamburgerOpen);
+    };
+
     return (
-        <div className="header">
-            <h1 className="header__title">
-                <span className="caps">Marvel</span>{" "}
-                <span className="border-top-bottom">Ranking</span>
-            </h1>
-            <nav className="header__nav">
-                <ul className="nav__ul">
-                    <li className="ul__li" onClick={onRankClick}>
-                        Rank
-                    </li>
-                    <li className="ul__li" onClick={onLeaderboardClick}>
-                        Leaderboard
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <header className="header" ref={header}>
+            {!hamburgerOpen && (
+                <h1 className="header__title">
+                    <span className="caps">Marvel</span>{" "}
+                    <span className="border-top-bottom">Ranking</span>
+                </h1>
+            )}
+            {hamburgerOpen && <h1> </h1>}
+
+            {(showNav || hamburgerOpen) && (
+                <nav className="header__nav">
+                    <ul className="nav__ul">
+                        <li className="ul__li" onClick={onRankClick}>
+                            Rank
+                        </li>
+                        <li className="ul__li" onClick={onLeaderboardClick}>
+                            Leaderboard
+                        </li>
+                    </ul>
+                </nav>
+            )}
+
+            {!showNav && (
+                <div className="header__hamburger" onClick={toggleHamburger}>
+                    <div
+                        className={
+                            hamburgerOpen
+                                ? "hamburger__icon--open"
+                                : "hamburger__icon"
+                        }
+                    >
+                        &#9776;
+                    </div>
+                </div>
+            )}
+        </header>
     );
 };
 
